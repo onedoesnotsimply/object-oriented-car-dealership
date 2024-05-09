fa0ed520087ecf3a8a9f94a1e7efea190ebfe01b
Emily Trifone <emilytrifone@gmail.com>
2024-05-09 14:14:43 -0400

    first commit

---
+++ ObjectOrientedCarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,84 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name=name;
+        this.address=address;
+        this.phone=phone;
+        inventory = new ArrayList<>();
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        // Return all Vehicles in the inventory ArrayList
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        // Allow the user to add a car to the dealership inventory
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+
+    }
+
+
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
